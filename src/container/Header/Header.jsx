36a7406f1193dc7c9c801a08ef0cf4204def4824
eyaaabid <eyaabid60@gmail.com>
2024-11-03 21:33:08 +0100

    navbar fixed

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -63,7 +63,7 @@ const Header = () => {
       whileInView={scaleVariants.whileInView}
       className="app__header-circles"
       >
-        {[images.flutter, images.react, images.node].map((circle, index) => (
+        {[images.flutter,images.node,images.react].map((circle, index) => (
         <div className="circle-cmp app__flex" key={`circle-${index}`}>
           <img src={circle} alt="profile_bg" />
         </div>
