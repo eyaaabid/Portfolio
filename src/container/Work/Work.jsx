a571831ac968d8627390809b8762adb58c9e806b
eyaaabid <eyaabid60@gmail.com>
2024-11-07 21:23:09 +0100

    see project done

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -1,38 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import { AiFillEye, AiFillGithub } from 'react-icons/ai';
 import { motion } from 'framer-motion';
-
-import { AppWrap ,MotionWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
-
 import './Work.scss';
 
 const Work = () => {
-
   const [activeFilter, setActiveFilter] = useState('All');
   const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
   const [works, setWorks] = useState([]);
   const [filterWork, setFilterWork] = useState([]);
-
+  const [selectedImage, setSelectedImage] = useState(null); // New state for the modal
 
   useEffect(() => {
-    const query ='*[_type == "works"]';
-
-    client.fetch(query)
-      .then((data) => {
-        setWorks(data);
-        setFilterWork(data);
-      })
-
-  }, [])
+    const query = '*[_type == "works"]';
+    client.fetch(query).then((data) => {
+      setWorks(data);
+      setFilterWork(data);
+    });
+  }, []);
 
   const handleWorkFilter = (item) => {
     setActiveFilter(item);
     setAnimateCard([{ y: 100, opacity: 0 }]);
-
     setTimeout(() => {
       setAnimateCard([{ y: 0, opacity: 1 }]);
-
       if (item === 'All') {
         setFilterWork(works);
       } else {
@@ -41,9 +33,17 @@ const Work = () => {
     }, 500);
   };
 
+  const openModal = (imgUrl) => {
+    setSelectedImage(imgUrl);
+  };
+
+  const closeModal = () => {
+    setSelectedImage(null);
+  };
+
   return (
     <>
-      <h2 className='head-text'>My Creative <span> Portfolio </span> Section </h2>
+      <h2 className='head-text'>My Creative <span>Portfolio</span> Section</h2>
 
       <div className='app__work-filter'>
         {['UI/UX', 'Web App', 'Mobile App', 'All'].map((item, index) => (
@@ -55,7 +55,6 @@ const Work = () => {
             {item}
           </div>
         ))}
-
       </div>
 
       <motion.div
@@ -72,20 +71,13 @@ const Work = () => {
                 transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
                 className='app__work-hover app__flex'
               >
-
-                <a href='work.projectLink' target='_blank' rel='noreferrer' >
-                  <motion.div
-                    whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.9] }}
-                    transition={{ duration: 0.25 }}
-                    className='app__flex'
-                  >
-                    <AiFillEye />
-
-                  </motion.div>
-
-                </a>
-                <a href='work.projectLink' target='_blank' rel='noreferrer' >
+                <div
+                  onClick={() => openModal(urlFor(work.imgUrl))} // Open modal with image URL
+                  className='app__flex'
+                >
+                  <AiFillEye />
+                </div>
+                <a href={work.projectLink} target='_blank' rel='noreferrer'>
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
@@ -93,31 +85,31 @@ const Work = () => {
                     className='app__flex'
                   >
                     <AiFillGithub />
-
                   </motion.div>
-
                 </a>
-
               </motion.div>
             </div>
             <div className='app__work-content app__flex'>
               <h4 className='bold-text'>{work.title}</h4>
-              <p className='p-text' style={{ marginTop: 10 }} >{work.description}</p>
+              <p className='p-text' style={{ marginTop: 10 }}>{work.description}</p>
               <div className='app__work-tag app__flex'>
                 <p className='p-text'>{work.tags[0]}</p>
-
               </div>
-
             </div>
-
-
           </div>
         ))}
-
       </motion.div>
+
+      {selectedImage && (
+        <div className='app__modal' onClick={closeModal}>
+          <div className='app__modal-content' onClick={(e) => e.stopPropagation()}>
+            <img src={selectedImage} alt='Enlarged work' />
+            <button onClick={closeModal}>Close</button>
+          </div>
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default AppWrap(MotionWrap(Work,'app__works'),'work','app__primarybg');
-
+export default AppWrap(MotionWrap(Work, 'app__works'), 'work', 'app__primarybg');
