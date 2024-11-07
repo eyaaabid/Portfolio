b1f548c981de00601b5360c63a60c7855c670849
eyaaabid <eyaabid60@gmail.com>
2024-11-07 15:48:12 +0100

    cv download fixed

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BsGithub, BsLinkedin } from 'react-icons/bs';
 import { AiOutlineDownload } from 'react-icons/ai';
+import { client } from '../client';
 
 const SocialMedia = () => {
+  const [cvUrl, setCvUrl] = useState('');
+
+  useEffect(() => {
+    const query = `*[_type == "cv"][0]{cvFile{asset->{url}}}`;
+    client.fetch(query).then((data) => {
+
+      if (data && data.cvFile && data.cvFile.asset && data.cvFile.asset.url) {
+        setCvUrl(data.cvFile.asset.url);
+      } else {
+        console.error("No CV URL found");
+      }
+    }).catch(error => console.error("Fetch error:", error));
+  }, []);
+
+
+  const handleDownload = () => {
+    if (cvUrl) {
+      window.open(cvUrl, '_blank');
+    } else {
+      console.error("CV URL is not available");
+    }
+  };
+
   return (
-    <div className='app__social'>
+    <div className="app__social">
       <div>
         <a href="https://github.com/eyaaabid" target="_blank" rel="noopener noreferrer">
           <BsGithub />
@@ -16,9 +40,9 @@ const SocialMedia = () => {
         </a>
       </div>
       <div>
-        <a href="/eyaabid__cv.pdf" download title="Download CV">
+        <button onClick={handleDownload} title="Download CV" style={{ background: 'none', border: 'none', cursor: 'pointer' }}>
           <AiOutlineDownload />
-        </a>
+        </button>
       </div>
     </div>
   );
